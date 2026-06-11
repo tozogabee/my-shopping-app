eed161aa8da746f3d3527a18b75fd6696829fd95
Gabor Toth <tozogabee@gmail.com>
2026-06-11 19:27:30 +0200

    Initial commit with .gitignore

---
+++ backend/booking-service/src/test/java/com/example/bookingservice/BookingServiceApplicationTests.java
@@ -0,0 +1,12 @@
+package com.example.bookingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
